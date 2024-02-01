a4fe7b294d7b67e0a90ba97b64e83cb34a1728fe
seungwan_cho <swcho8220@gmail.com>
2024-02-01 22:28:22 +0900

    [bugfix] Thumnail size bugfix
    - between jpg and gif has different size
    - match the size by changing padding to margin

--- app/page.tsx
+++ app/page.tsx
@@ -4,41 +4,41 @@ import itemList from './itemList.json';
 
 export default function Home() {
   return (
-    <main className='flex flex-wrap items-center p-12 max-w-[1000px] m-auto'>
+    <main className="flex flex-wrap items-center p-12 max-w-[1000px] m-auto">
       {itemList.map((item) => (
         // wrapper
         <div
-          className='w-1/3 flex justify-center items-center p-2'
+          className="w-1/3 flex justify-center items-center p-2"
           key={item.title}
         >
           {/* item button */}
           <Link
-            className='w-full h-fit border-solid border-2 border-gray-400 p-1 group'
+            className="w-full h-fit border-solid border-2 border-gray-400 p-1 group"
             href={item.href}
           >
             {/* title */}
-            <p className='text-xl text-center font-bold p-1'>{item.title}</p>
+            <p className="text-xl text-center font-bold p-1">{item.title}</p>
 
             {/*
               images
               first child: static placeholder
               second child: gif that plays when hover
             */}
-            <div className='relative h-[200px] p-1'>
+            <div className="relative h-[12rem] m-2">
               <Image
                 src={`/images${item.href}/thumbnail.jpg`}
                 width={500}
                 height={300}
                 style={{ objectFit: 'cover', height: '100%' }}
-                alt='item thumbnail'
+                alt="item thumbnail"
               />
               <Image
                 src={`/images${item.href}/thumbnail.gif`}
                 width={500}
                 height={300}
                 style={{ objectFit: 'cover', height: '100%' }}
-                className='absolute top-0 left-0 opacity-0 group-hover:opacity-100'
-                alt='item thumbnail'
+                className="absolute top-0 left-0 opacity-0 group-hover:opacity-100"
+                alt="item thumbnail"
               />
             </div>
           </Link>
