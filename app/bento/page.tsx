3226f74ddcf344e078340c2a1fdfbb1f1ef18ed0
seungwan_cho <swcho8220@gmail.com>
2024-02-01 22:26:00 +0900

    [feat] Add glowing bento box item
    - add glowing bento box which is from personal portfolio
    - highlight hovered box and dehighlight others
    - highlight by glowing effect that follows the mouse pointer
    - install sass to use scss

---
+++ app/bento/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useState } from 'react';
+import Instruction from '../components/Instruction';
+import styles from './Bento.module.scss';
+import GlowOnHoverBox from './GlowOnHoverBox';
+
+export default function Bento() {
+  const [className, setClassName] = useState('');
+
+  return (
+    <>
+      {/* instruction */}
+      <Instruction
+        instructions={[
+          'When the box is hovered, it is highlighted with the glow effect that follows your mouse pointer.',
+          'If one of the boxes is highlighted, others are dehighlighted.',
+        ]}
+      />
+
+      {/* bento box */}
+      <div className="p-4 mt-4">
+        <div
+          className={`${styles.grid} ${className}`}
+          onMouseEnter={() => setClassName(styles.select)}
+          onMouseLeave={() => setClassName('')}
+        >
+          {new Array(5).fill('').map((_, idx) => (
+            <GlowOnHoverBox>{idx + 1}</GlowOnHoverBox>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
