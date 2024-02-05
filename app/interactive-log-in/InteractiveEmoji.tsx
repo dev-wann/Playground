746c9325376080024c47415c30952d52c4e067d9
seungwan_cho <swcho8220@gmail.com>
2024-02-06 00:37:38 +0900

    [feat] Implmenet facial expression change
    - apply facial expression to idle, editing, success, fail status
    - add exhastive check to log in validation

--- app/interactive-log-in/InteractiveEmoji.tsx
+++ app/interactive-log-in/InteractiveEmoji.tsx
@@ -1,20 +1,12 @@
+import { StatusEnum } from './page';
+
 type Props = {
   userID: string;
-  success: boolean;
+  status: StatusEnum;
   error: string | null;
 };
 
-export default function InteractiveEmoji({ userID, success, error }: Props) {
-  // message
-  let message: string | React.JSX.Element = 'Hello there!';
-  if (success)
-    message = (
-      <>
-        Welcome <b>{userID}</b>
-      </>
-    );
-  if (error) message = <b>{error}</b>;
-
+export default function InteractiveEmoji({ userID, status, error }: Props) {
   // eyes
   const eyesIdle = (
     <>
@@ -34,10 +26,10 @@ export default function InteractiveEmoji({ userID, success, error }: Props) {
   );
   const eyesFail = (
     <>
-      <div className="absolute top-[22%] left-[22%] text-5xl text-yellow-950 text-center font-black">
+      <div className="absolute top-[25%] left-[20%] text-5xl text-yellow-950 text-center font-black">
         X
       </div>
-      <div className="absolute top-[22%] right-[22%] text-5xl text-yellow-950 text-center font-black">
+      <div className="absolute top-[25%] right-[20%] text-5xl text-yellow-950 text-center font-black">
         X
       </div>
     </>
@@ -50,13 +42,48 @@ export default function InteractiveEmoji({ userID, success, error }: Props) {
   const mouthEdit = (
     <div className="absolute w-[20%] pt-[25%] left-[40%] top-[60%] rounded-full bg-yellow-950" />
   );
+  const mouthFail = (
+    <div className="absolute w-[40%] pt-[5%] left-[30%] top-[70%] bg-yellow-950" />
+  );
+
+  // set message & facial expressions
+  let message;
+  let eyes, mouth;
+  switch (status) {
+    case StatusEnum.EDITING:
+      message = '...';
+      eyes = eyesIdle;
+      mouth = mouthEdit;
+      break;
+    case StatusEnum.SUCCESS:
+      message = (
+        <>
+          Welcome <b>{userID}</b>
+        </>
+      );
+      eyes = eyesSuccess;
+      mouth = mouthIdle;
+      break;
+    case StatusEnum.FAIL:
+      message = <b>{error}</b>;
+      eyes = eyesFail;
+      mouth = mouthFail;
+      break;
+    default: // IDLE
+      message = 'Hello there!';
+      eyes = eyesIdle;
+      mouth = mouthIdle;
+      break;
+  }
 
   // render
   return (
     <>
       {/* message */}
       <div className="w-4/5 p-1 m-auto mb-6 bg-white text-black text-center rounded relative">
-        <p className={error ? 'text-red-500' : ''}>{message}</p>
+        <p className={status === StatusEnum.FAIL ? 'text-red-500' : ''}>
+          {message}
+        </p>
         <div className="absolute top-full right-[15%] border-solid border-8 border-transparent border-t-white" />
       </div>
 
@@ -65,14 +92,8 @@ export default function InteractiveEmoji({ userID, success, error }: Props) {
         {/* background */}
         <div className="absolute w-full h-full top-0 left-0 top bg-yellow-400 rounded-full" />
 
-        {/* eyes */}
-        {eyesIdle}
-        {/* {eyesSuccess} */}
-        {/* {eyesFail} */}
-
-        {/* mouth */}
-        {mouthIdle}
-        {/* {mouthEdit} */}
+        {eyes}
+        {mouth}
       </div>
     </>
   );
