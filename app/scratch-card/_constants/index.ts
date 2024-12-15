880382e946e2ada40bc5cb788910f6dc876b08c0
Carl Cho <carl.cho@buzzvil.com>
2024-12-15 21:09:19 +0900

    [feat] implement scratch card controller
    - canvasController
      - controls scratch effect triggered by mouse drag
    - useScratchCardController
      - manage canvas controller instance
      - manage various states related to scratch card
      - offers control methods
    - add constants and cover image

--- app/scratch-card/_constants/index.ts
+++ app/scratch-card/_constants/index.ts
@@ -1,5 +1,10 @@
 export enum ScratchCardStatus {
   IDLE = "IDLE",
+  READY = "READY",
   PENDING = "PENDING",
   COMPLETED = "COMPLETED",
 }
+
+export const SCRATCH_RADIUS = 20;
+export const SCRATCH_CANVAS_ID = "scratch-card-canvas";
+export const SCRATCH_COVER_IMAGE_SRC = "/images/scratch-card/cover.png";
